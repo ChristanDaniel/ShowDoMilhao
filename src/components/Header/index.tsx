74a333e449e78dbf4fe9d9614eabbe2426238fec
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-15 14:30:37 -0300

    create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Headers } from './styles'
+
+const Header = (): JSX.Element => {
+  return (
+    <>
+      <Headers>
+        <h1>Olá</h1>
+      </Headers>
+    </>
+  )
+}
+
+export default Header
