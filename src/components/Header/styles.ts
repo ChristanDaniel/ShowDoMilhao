5a3c9852f5ddd11a512f9138362b74c180fe8021
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-15 14:31:02 -0300

    crate css Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Headers = styled.header`
+  background-color: #e2e2e2;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: auto;
+`
+
+export { Headers }
