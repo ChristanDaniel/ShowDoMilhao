28abd3200500b478db31165d395d8c3e59d43632
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-17 14:17:21 -0300

    css OptionsPage

--- src/features/OptionsPage/styles.ts
+++ src/features/OptionsPage/styles.ts
@@ -10,6 +10,14 @@ const OptionsPageContainer = styled.main`
   /* justify-content: center; */
   flex-direction: column;
 
+  h1 {
+    margin-bottom: 16px;
+    color: #FFFFFF;
+  }
+
+  button {
+    margin-top: 50px
+  }
 `
 
 const OptionsContent = styled.div`
@@ -22,5 +30,30 @@ const OptionsContent = styled.div`
   background: #00008B;
 `
 
+const SelectContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    margin: 10px;
+  }
+
+  input {
+    width: 272px;
+  }
+
+  p {
+    color: #FFFFFF;
+    font-size: 20px;
+    text-transform: capitalize;
+    padding-right: 100px;
+  }
+`
+
+const Select = styled.select`
+  width: 272px;
+`
 
 export { OptionsPageContainer, OptionsContent, SelectContainer, Select }
