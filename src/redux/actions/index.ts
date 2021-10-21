f8a08ed66adcb83d6a2669631f8bb1e596a1e407
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-21 13:04:42 -0300

    add Action sendOptionsCategory

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,7 +1,12 @@
 export const SEND_USER_INFO = 'SEND_USER_INFO';
+export const SEND_OPTIONS_CATEGORY_INFO = 'SEND_OPTIONS_CATEGORY_INFO';
 
 
 
-export const sendUserInfo = (name: string, email: string) => ({
+export const sendUserInfo = (name:string, email: string) => ({
   type: SEND_USER_INFO, name, email
+});
+
+export const sendOptionsCategory = ( id:string ) => ({
+  type: SEND_OPTIONS_CATEGORY_INFO, id
 });
