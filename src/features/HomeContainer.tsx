68f2e4238481ae533697ed166d45143fe4066bdd
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-22 14:02:42 -0300

    pegando dado do redux

--- src/features/HomeContainer.tsx
+++ src/features/HomeContainer.tsx
@@ -1,15 +1,40 @@
 import router from 'next/dist/client/router'
 import React, { useState } from 'react'
 import Button from '../components/Button'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { SEND_USER_INFO } from '../redux/actions'
 
 import { Container, InputContainer, ButtonContainer } from './styles'
 
-const HomeContainer = (): JSX.Element => {
-  const [nickname, setNickname] = useState('')
-  const [email, setEmail] = useState('')
+// function teste2(name: string, email: string) {
+//   return { type: SEND_USER_INFO, name, email}
+// }
 
-  const handleChangeNickname = async (valueName: string) => {
-    setNickname(valueName)
+
+type Teste123 = {
+  id: string
+}
+
+const HomeContainer = (): JSX.Element => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [testeee, setTesteee] = useState('')
+
+
+  const dispatch = useDispatch()
+
+  const getTest = async () => {
+    dispatch({ type: SEND_USER_INFO, name, email})
+
+    router.push(`/game`)
+
+  }
+  const porfavor = useSelector(state => state.rootReducer.OptionsCategory.category)
+  console.log(porfavor)
+
+  const handleChangeName = async (valueName: string) => {
+    setName(valueName)
   }
 
   const handleChangeEmail = async (valueEmail: string) => {
@@ -20,18 +45,18 @@ const HomeContainer = (): JSX.Element => {
   return (
     <>
       <Container>
+        {/* <h1>{category1}</h1> */}
         <img src="/ShowDoMilhãoLogo.png" alt="Logo" />
 
         <InputContainer>
-          <input placeholder="Nome" onChange={(event) => handleChangeNickname(event.target.value)} value={nickname} />
+          <input placeholder="Nome" onChange={(event) => handleChangeName(event.target.value)} value={name} />
           <input placeholder="E-Mail" onChange={(event) => handleChangeEmail(event.target.value)} value={email} />
         </InputContainer>
 
         <ButtonContainer>
-          <Button>Jogar</Button>
+          <Button onClick={getTest}>Jogar</Button>
           <Button onClick={() => router.push(`/options`)}>Editar Perguntas</Button>
         </ButtonContainer>
-
       </Container>
     </>
   )
