a0c77efaaf68766eaca073deb282eb8ad4727ceb
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-25 15:13:49 -0300

    trying render add QuestionButton

--- src/components/QuestionButton/index.tsx
+++ src/components/QuestionButton/index.tsx
@@ -1,12 +1,55 @@
-import { ButtonHTMLAttributes } from 'react'
+import { ButtonHTMLAttributes, Children, useEffect, useState } from 'react'
 import React from 'react'
 
-import { QuestionButtons } from './styles'
+import { QuestionsButtons } from './styles'
+import Button from '../Button'
+// import { useSelector } from 'react-redux'
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
-
-const QuestionButton = ({ ...props }: ButtonProps) => {
-  return <QuestionButtons {...props} />
+// type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+interface QuestionButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  incorrectAnswers: string[]
+  correctAnswer: string
 }
 
-export default QuestionButton
+type Teste000 = {
+  Questionsfinalizada: []
+}
+
+const QuestionButtons = ({ incorrectAnswers, correctAnswer, ...props }: QuestionButtonProps) => {
+  // const [teste123, setTeste123] = useState([])
+  // // const handleClassButton = async (incorrectAnswers: string, correctAnswer: string) => {
+  // //     console.log('TESTE1', incorrectAnswers)
+  // //     console.log('TESTE2', correctAnswer)
+  // // }
+
+  // useEffect(() => {
+
+  //   console.log(incorrectAnswers)
+  //   setTeste123(TodasPerguntas)
+  // }, [])
+
+  // onClick={() => handleClassButton(incorrectAnswers, correctAnswer)}
+  // return (
+  return (
+          <Button  {...props}>
+          </Button>
+  )
+
+  // {teste123.map((wrong_answers, index) => {
+  //   return (
+  //     <div key={index}>
+
+  //       <QuestionsButtons {...props}
+  //         incorrectAnswers
+  //         correctAnswer
+  //       >
+  //         {wrong_answers}
+  //       </QuestionsButtons>
+
+  //     </div>
+
+  //   )
+  // })}
+}
+
+export default QuestionButtons
