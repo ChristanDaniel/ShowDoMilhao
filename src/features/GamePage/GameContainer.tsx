027a59216a06e8b53ae420a98eb7edb81d597dc6
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-25 15:16:06 -0300

    pequenas alterações

--- src/features/GamePage/GameContainer.tsx
+++ src/features/GamePage/GameContainer.tsx
@@ -3,6 +3,9 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Button from '../../components/Button'
+import QuestionsButton from '../../components/QuestionsButton'
+import { QuestionButtons, QuestionsButtons } from '../../components/QuestionsButton/styles'
+
 // import Button from '../../components/Button'
 import { GamerContainer, HeaderGameContainer, BodyGameContainer, QuestionsGameContainer, QuestionsCategoryContent } from './styles'
 
@@ -12,7 +15,6 @@ type AllQuestions = {
   category?: string
   correct_answer?: string
   difficulty?: string
-  incorrect_answers: string[]
   question?: string
 }
 
@@ -23,23 +25,31 @@ type Teste123 = [{
 
 const GameContainer = (): JSX.Element => {
   const [getAllQuestions, setGetAllQuestions] = useState<AllQuestions[]>([])
-  const [teste123, setTeste123] = useState([])
+  // const [teste123, setTeste123] = useState([])
+  // const [groupingQuestions, setGroupingQuestions] = useState([])
 
   const AllQuestions = async () => {
     const response = await axios(`https://opentdb.com/api.php?amount=10`)
     console.log(response)
 
     const testeMilagroso = response.data.results
-    const teste1233 = response.data.results.incorrect_answers
-
     setGetAllQuestions(testeMilagroso)
 
-    setTeste123(teste1233)
-
+    // setTeste123(teste1233)
+    // setGroupingQuestions(response.data.results)
+    // console.log('TESTE GROUPING', groupingQuestions)
   }
 
 
   const User: Teste123 = useSelector((state) => state.rootReducer.game.user)
+
+
+
+  // const listOrdenada = (incorrect_answers: string[]) => {
+  //   // return pokeList.sort((a, b) => a.id - b.id)
+  //   return incorrect_answers[Math.floor(Math.random()*incorrect_answers.length)]
+  // }
+
 
 
   useEffect(() => {
@@ -63,6 +73,7 @@ const GameContainer = (): JSX.Element => {
         <BodyGameContainer>
           <div>
           {getAllQuestions.map((AllQuestion, index) => {
+            const TodasPerguntas = incorrectAnswers.push(correctAnswer)
             return (
               <>
                 <QuestionsGameContainer key={AllQuestion.id}>
@@ -71,13 +82,19 @@ const GameContainer = (): JSX.Element => {
                     <span>Categoria: {AllQuestion.category}</span>
                   </QuestionsCategoryContent>
                   <p>{AllQuestion.question}</p>
+                  {/* <QuestionsButton
+                    incorrectAnswers={AllQuestion.incorrect_answers}
+                    correctAnswer={AllQuestion.correct_answer}
+                  >{AllQuestion.incorrect_answers}</QuestionsButton> */}
                   <div>
                     <Button>{AllQuestion.correct_answer}</Button>
                     {console.log('asdasd', AllQuestion.incorrect_answers)}
                     {AllQuestion.incorrect_answers.map((wrong_answer, index) => {
                       return (
                         <>
-                          <Button key={AllQuestion.id + index.toString()}>{wrong_answer}</Button>
+                          <QuestionsButton key={AllQuestion.id + index.toString()}
+                            incorrectAnswers={AllQuestion.incorrect_answers}
+                            correctAnswer={AllQuestion.correct_answer}>{TodasPerguntas}</QuestionsButton>
                         </>
                       )
                     })}
