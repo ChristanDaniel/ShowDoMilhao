392a020d45ac5f4989c5cbf1be9700fc595e9bcc
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-27 21:01:07 -0300

    pequenas alterações

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
 
+import { Provider } from 'react-redux'
+import store from '../src/redux/store'
+
 import '../styles/Global'
 import { GlobalStyle } from '../styles/Global'
-
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-     <GlobalStyle />
-     <Component {...pageProps} />
+      <GlobalStyle />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </>
   )
 }
