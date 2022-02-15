ee0141bdf72ad741c275ffa96de7f306ae1417b1
ChristianDaniel <christian_daniel04@hotmail.com>
2022-02-15 18:57:42 -0300

    create getlistquestionsCategory

--- src/services/GamePage/PageListService.ts
+++ src/services/GamePage/PageListService.ts
@@ -1,5 +1,5 @@
 import { api } from '../api'
-import { BASE_URL } from '../apiConstants'
+import { BASE_URL, BASE_URL_CATEGORY } from '../apiConstants'
 import { getApiResponseProps } from './PageModel'
 
 // export const getUsers = async (amount:string, category:string) => {
@@ -22,3 +22,17 @@ export const getListQuestions = async (): Promise<getApiResponseProps> => {
     throw new Error('Fail handleRequest getListQuestions')
   }
 }
+
+export const getListQuestionsCategory = async (categoryName: string): Promise<getApiResponseProps> => {
+  try {
+    const { data } = await api.get<getApiResponseProps>(BASE_URL_CATEGORY, {
+      params: { categoryName }
+    })
+
+    return data
+  } catch (error) {
+    throw new Error('Fail handleRequest getListQuestionsCategory')
+  }
+}
+
+
--- src/services/apiConstants.ts
+++ src/services/apiConstants.ts
@@ -1,6 +1,6 @@
 const BASE_URL = `?amount=10`
 const BASE_URL_AMOUNT = `?amount`
-const BASE_URL_CATEGORY = `?category`
+const BASE_URL_CATEGORY = `?category=`
 const BASE_URL_DIFFICULTY = `?difficulty`
 
 
